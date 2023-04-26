2fe8f7250edbc0726f18ddf042e0909b36cb91da
sodmaq <sodmaq@gmail.com>
2023-04-26 02:29:56 -0700

    shell

--- main.c
+++ main.c
@@ -1,76 +1,73 @@
 #include "main.h"
 
 /**
- * free_data - releases memory used by data structure
+ * free_data - frees data structure
  *
- * @datash: pointer to data structure
- * Return: void
+ * @datash: data structure
+ * Return: no return
  */
 void free_data(data_shell *datash)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; datash->_environ[i]; i++)
-    {
-        free(datash->_environ[i]);
-    }
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
+	}
 
-    free(datash->_environ);
-    free(datash->pid);
+	free(datash->_environ);
+	free(datash->pid);
 }
 
 /**
- * set_data - initializes data structure
+ * set_data - Initialize data structure
  *
- * @datash: pointer to data structure
+ * @datash: data structure
  * @av: argument vector
- * Return: void
+ * Return: no return
  */
 void set_data(data_shell *datash, char **av)
 {
-    unsigned int i;
+	unsigned int i;
 
-    datash->av = av;
-    datash->input = NULL;
-    datash->args = NULL;
-    datash->status = 0;
-    datash->counter = 1;
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
 
-    for (i = 0; environ[i]; i++)
-        ;
+	for (i = 0; environ[i]; i++)
+		;
 
-    datash->_environ = malloc(sizeof(char *) * (i + 1));
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
 
-    for (i = 0; environ[i]; i++)
-    {
-        datash->_environ[i] = _strdup(environ[i]);
-    }
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
 
-    datash->_environ[i] = NULL;
-    datash->pid = aux_itoa(getpid());
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
 }
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
  * @ac: argument count
  * @av: argument vector
  *
- * Return: 0 on success
+ * Return: 0 on success.
  */
 int main(int ac, char **av)
 {
-    data_shell datash;
-    (void) ac; // Avoids unused variable warning
+	data_shell datash;
+	(void) ac;
 
-    signal(SIGINT, get_sigint);
-    set_data(&datash, av);
-    shell_loop(&datash);
-    free_data(&datash);
-
-    if (datash.status < 0)
-        return (255); // Returns max value of unsigned char
-    else
-        return (datash.status);
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
-
